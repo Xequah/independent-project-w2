5ce8bafdbc6c1b16f6208096bf44c354dfdb64db
cr <xequah@yahoo.com>
2018-10-19 14:50:32 -0700

    add a button with an alert attached when pressed it would bring up the top3 langguages used for that piticular field.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
-var language = $("input: radio[name=language]: checked").val();
+$(function(){
+    $('input[type="button"]').click(function(){
+        alert($('input[type=radio]:checked').val());
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var language = $("input: radio[name=language]: checked").val();
